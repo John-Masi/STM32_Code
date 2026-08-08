a4f1eeb4f002a88dbe7b141a85a6a2953e938a60
John Masi <johnrmasi@gmail.com>
2026-08-08 16:54:08 -0500

    Update gpio.cpp

--- GPIO/src/gpio.cpp
+++ GPIO/src/gpio.cpp
@@ -1,12 +1,6 @@
 #include "../Include/gpio.hpp"
 
-void GPIO::gpio_init(void) {
-    RCC->AHB1ENR |= (1 << GPIOAEN) | (1 << GPIOCEN);
-    GPIOA->MODER |= (2 << (9 * 2));
-    GPIOA->AFRL |= (7 << 4);
-}
-
-void GPIO::button_init(void) {
+void GPIO<GPIOB>::button_init(void) {
     RCC->AHB1ENR |= (1 << GPIOBEN); 
     RCC->AHB1ENR |= (1 << GPIOCEN);
     RCC->APB2ENR |= (1 << SYSCFGEN);
@@ -14,7 +8,7 @@ void GPIO::button_init(void) {
     GPIOC->MODER &= BTTN_PIN;
 
     for(int i = 0; i < 3; i++) {
-        GPIOB->MODER |= (1 << (i * 2));
+        gpio->MODER |= (1 << (i * 2));
     }
 
     SYSCFG->EXTICR4 |= (0x2 << 4);
@@ -25,21 +19,18 @@ void GPIO::button_init(void) {
     NVIC->ISER0[1] |= (1 << 8);
 }
 
-// Turn off all gpiob pins
-void GPIO::turn_off(void) {
+void GPIO<GPIOB>::turn_off(void) {
     for(int i = 0; i < 4; i++) {
-        GPIOB->ODR &= ~(1 << i);
+        gpio->ODR &= ~(1 << i);
     }
 }
 
-// Turn on all 4 gpiob pins 
-void GPIO::turn_on(void) {
+void GPIO<GPIOB>::turn_on(void) {
     for(int i = 0; i < 4; i++) {
-        GPIOB->ODR |= (1 << i);
+        gpio->ODR |= (1 << i);
     }
 }
-
-void GPIO::led_on(uint8_t pin) {
-    GPIOB->ODR &= (1 << pin);
-    GPIOB->ODR |= (1 << pin);
+void GPIO<GPIOB>::led_on(uint8_t pin) {
+    gpio->ODR &= (1 << pin);
+    gpio->ODR |= (1 << pin);
 }
