d38fc291193cba3358534e295450c390daf3d1cb
John Masi <johnrmasi@gmail.com>
2026-08-08 16:53:08 -0500

    Update gpio.hpp

--- GPIO/Include/gpio.hpp
+++ GPIO/Include/gpio.hpp
@@ -1,6 +1,6 @@
 #include <cstdint>
 #include <array> 
-#include "typedef.hpp"
+#include "gpio_typedef.hpp"
 #include "../syscfg-nvic-rcc/include/rcc.hpp"
 #include "../syscfg-nvic-rcc/include/nvic.hpp"
 #include "../syscfg-nvic-rcc/include/syscfg.hpp"
@@ -9,21 +9,24 @@
 #ifndef GPIO_HPP
 #define GPIO_HPP
 
+template<uintptr_t BASE>
 class GPIO {
     public:
-        GPIO() {
-            //gpio_init();
-        }
-        void gpio_init(void);
+        GPIO() = default;
+    private:
+        static constepxr auto gpio = reinterpret_cast<GPIO_Typedef*>(BASE);
+};
 
+// 
+class GPIO<GPIOB> {    
+    public:
         void button_init(void); // Init for cascading-leds
-
         void turn_off(void);
         void turn_on(void);
         void led_on(uint8_t pin);
 
     private:
-
+         GPIO_Typedef* gpio = reinterpret_cast<GPIO_Typedef*>(GPIOB);
 };
 
 #endif 
