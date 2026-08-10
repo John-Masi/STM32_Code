af8573543ad7549847b26d44e48031e36c506cbd
John Masi <johnrmasi@gmail.com>
2026-08-09 19:56:18 -0500

    Update i2c.hpp

--- I2C/include/i2c.hpp
+++ I2C/include/i2c.hpp
@@ -1,28 +1,13 @@
 #include <cstdint>
+#include "i2c_typedef.hpp"
 
 #ifndef I2C_HPP
 #define I2C_HPP
 
-struct I2C_Typedef {
-    volatile uint32_t CR1;
-	volatile uint32_t CR2;
-	volatile uint32_t OAR1;
-	volatile uint32_t OAR2;
-	volatile uint32_t DR;
-	volatile uint32_t SR1;
-	volatile uint32_t SR2;
-	volatile uint32_t CCR;
-	volatile uint32_t TRISE;
-};
-
-#define I2C1 ((I2C_Typedef *)0X40005400)
-
+template <uintptr_t BASE>
 class I2C {
     public:
-        I2C(I2C_Typedef& i2c) {
-            I2C_ptr = &i2c; 
-            i2c_init();
-        }
+        I2C() = default;
 
         ~I2C() = default;
 
@@ -30,7 +15,7 @@ class I2C {
     void i2c_init(void);
 
     private:
-        I2C_Typedef* I2C_ptr{nullptr};
+        static constexpr auto i2c = reinterpret_cast<I2C_Typedef*>(BASE);
 
 };
 
