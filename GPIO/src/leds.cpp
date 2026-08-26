bac2154f141b58e98232be734e9cc8a91f39d273
John Masi <johnrmasi@gmail.com>
2026-08-25 21:58:01 -0500

    Update leds.cpp

--- GPIO/src/leds.cpp
+++ GPIO/src/leds.cpp
@@ -1,15 +1,15 @@
 #include "../Include/leds.hpp"
 
 template <uintptr_t ADDR1,uintptr_t ADDR2>
-void LEDS<ADDR1,ADDR2>::button_init(void) {
-    RCC->AHB1ENR |= (1 << GPIOBEN); 
+void LEDS<ADDR1,ADDR2>::button_init(uint8_t GPIOEN) {
+    RCC->AHB1ENR |= (1 << GPIOEN); 
     RCC->AHB1ENR |= (1 << GPIOCEN);
     RCC->APB2ENR |= (1 << SYSCFGEN);
 
-    gpio_c->MODER &= BTTN_PIN;
+    gpio_2->MODER &= BTTN_PIN;
 
     for(int i = 0; i < 3; i++) {
-        gpio_b->MODER |= (1 << (i * 2));
+        gpio_1->MODER |= (1 << (i * 2));
     }
 
     SYSCFG->EXTICR4 |= (0x2 << 4);
@@ -23,19 +23,25 @@ void LEDS<ADDR1,ADDR2>::button_init(void) {
 template <uintptr_t ADDR1,uintptr_t ADDR2>
 void LEDS<ADDR1,ADDR2>::turn_off(void) {
     for(int i = 0; i < 4; i++) {
-        gpio_b->ODR &= ~(1 << i);
+        gpio_1->ODR &= ~(1 << i);
     }
 }
 
 template <uintptr_t ADDR1,uintptr_t ADDR2>
 void LEDS<ADDR1,ADDR2>::turn_on(void) {
     for(int i = 0; i < 4; i++) {
-        gpio_b->ODR |= (1 << i);
+        gpio_1->ODR |= (1 << i);
     }
 }
 
 template <uintptr_t ADDR1,uintptr_t ADDR2>
 void LEDS<ADDR1,ADDR2>::led_on(uint8_t pin) {
-    gpio_b->ODR &= (1 << pin);
-    gpio_b->ODR |= (1 << pin);
+    gpio_1->ODR &= (1 << pin);
+    gpio_1->ODR |= (1 << pin);
 }
+
+template <uintptr_t ADDR1,uintptr_t ADDR2>
+void LEDS<ADDR1,ADDR2>::led_on(uint8_t pin) {
+    gpio_1->ODR &= ~(1 << pin);
+}
+
