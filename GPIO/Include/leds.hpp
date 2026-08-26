5ddf128cb3b84a35c9707b63c4da9ad4a204439d
John Masi <johnrmasi@gmail.com>
2026-08-25 21:58:35 -0500

    Update leds.hpp

--- GPIO/Include/leds.hpp
+++ GPIO/Include/leds.hpp
@@ -12,15 +12,15 @@
 template <uintptr_t ADDR1,uintptr_t ADDR2>
 class LEDS{    
     public:
-        void button_init(void); // Init for cascading-leds
+        void button_init(uin8_t GPIOEN); 
         void turn_off(void);
         void turn_on(void);
         void led_on(uint8_t pin);
         void led_off(uint8_t pin);
 
     private:
-         inline static auto gpio_b = reinterpret_cast<GPIO_Typedef*>(ADDR1);
-         inline static auto gpio_c = reinterpret_cast<GPIO_Typedef*>(ADDR2);
+         inline static auto gpio_1 = reinterpret_cast<GPIO_Typedef*>(ADDR1);
+         inline static auto gpio_2 = reinterpret_cast<GPIO_Typedef*>(ADDR2);
 };
 
 #endif 
